d84b0ec90854a2c9f469423f6c868f12d271e47b
plamen-yoan <plamen_yoan@hotmail.com>
2015-07-06 15:03:49 +0300

    MenuStateUpdated

--- JumpOnAHeadGame/Controller/States/MenuState.cs
+++ JumpOnAHeadGame/Controller/States/MenuState.cs
@@ -8,6 +8,8 @@
 
     public class MenuState : State
     {
+        private int menuId;
+
         public MenuState(State nextState)
             : base(nextState)
         {
@@ -19,20 +21,49 @@
             this.SpritesInState.Add(UIInitializer.OptionsButton.Sprite);
             this.SpritesInState.Add(UIInitializer.CreditsButton.Sprite);
             this.SpritesInState.Add(UIInitializer.ExitButton.Sprite);
+
+            this.MenuId = 1;
         }
 
+        private int MenuId
+        {
+            get
+            {
+                return this.menuId;
+            }
+            set
+            {
+                if (value < 5 && value > 0)
+                {
+                    this.menuId = value;
+                }
+            }
+        }
         private bool IsDone { get; set; }
 
         public override void Execute()
         {
             if (!this.IsDone)
             {
+
                 this.NextState = this;
 
                 SoundManager.Play("MenuSound");
 
-                bool mouseStartHover = UIInitializer.StartButton.Sprite.CollisionRectangle.Contains(Mouse.GetState().X, Mouse.GetState().Y);
-                if (mouseStartHover)
+                foreach (KeyboardButtonState key in InputHandler.ActiveKeys)
+                {
+                    if (key.Button == Keys.Down && key.ButtonState == KeyboardButtonState.KeyState.Clicked)
+                    {
+                        MenuId++;
+                    }
+                    if (key.Button == Keys.Up && key.ButtonState == KeyboardButtonState.KeyState.Clicked)
+                    {
+                        MenuId--;
+                    }
+
+                }
+
+                if (MenuId == 1)
                 {
                     UIInitializer.StartButton.ChangeToHoverImage();
                 }
@@ -41,18 +72,43 @@
                     UIInitializer.StartButton.ChangeToNormalImage();
                 }
 
-                if (Mouse.GetState().LeftButton == ButtonState.Pressed && mouseStartHover)
+                foreach (KeyboardButtonState key in InputHandler.ActiveKeys)
                 {
-                    this.IsDone = true;
-                    SoundManager.Stop("MenuSound");
-                    SoundManager.Play("Sound" + Globals.rng.Next(1, 7).ToString());
-                    this.NextState = new UpdateState(this);
+                    if (MenuId == 1 && Keyboard.GetState().IsKeyDown(Keys.Enter) && key.ButtonState == KeyboardButtonState.KeyState.Clicked)
+                    {
+                        this.IsDone = true;
+                        SoundManager.Stop("MenuSound");
+                        SoundManager.Play("Sound" + Globals.rng.Next(1, 7).ToString());
+                        this.NextState = new UpdateState(this);
 
-                    StateMachine.CurrentLevel.Initialize();
+                        StateMachine.CurrentLevel.Initialize();
+                    }
+                }
+                if (MenuId == 2)
+                {
+                    UIInitializer.OptionsButton.ChangeToHoverImage();
+                }
+                else
+                {
+                    UIInitializer.OptionsButton.ChangeToNormalImage();
+                }
+                if (MenuId == 2 && Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
                 }
 
-                bool mouseExitHover = UIInitializer.ExitButton.Sprite.CollisionRectangle.Contains(Mouse.GetState().X, Mouse.GetState().Y);
-                if (mouseExitHover)
+                if (MenuId == 3)
+                {
+                    UIInitializer.CreditsButton.ChangeToHoverImage();
+                }
+                else
+                {
+                    UIInitializer.CreditsButton.ChangeToNormalImage();
+                }
+                if (MenuId == 3 && Keyboard.GetState().IsKeyDown(Keys.Enter))
+                {
+                }
+
+                if (MenuId == 4)
                 {
                     UIInitializer.ExitButton.ChangeToHoverImage();
                 }
@@ -60,17 +116,16 @@
                 {
                     UIInitializer.ExitButton.ChangeToNormalImage();
                 }
-
-                if (Mouse.GetState().LeftButton == ButtonState.Pressed && mouseExitHover)
+                if (MenuId == 4 && Keyboard.GetState().IsKeyDown(Keys.Enter))
                 {
                     this.IsDone = true;
                     SoundManager.Stop("MenuSound");
                     // TODO: EXIT
                     Game1.self.Exit();
-                    
                 }
             }
         }
+
 
         public override void Draw(AbstractRenderer renderer)
         {
