f936f3d49809f4bad69042459499c0dbefdd06ef
Sickname <xstefan44@gmail.com>
2015-07-06 18:43:00 +0300

    Level3

--- JumpOnAHeadGame/Controller/StateMachine.cs
+++ JumpOnAHeadGame/Controller/StateMachine.cs
@@ -25,7 +25,7 @@
             InitialState.NextState = MenuState;
 
             CurrentState = InitialState;
-            CurrentLevel = new LevelTwo();
+            CurrentLevel = new LevelThree();
             CurrentLevel.Initialize();
         }
 
